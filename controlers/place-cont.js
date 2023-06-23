c313847a22220a52918908f0825bfbcf7aa512b0
lorgical-mind <103314852+lorgical-mind@users.noreply.github.com>
2023-06-23 17:51:04 +0530

    Update place-cont.js

--- controlers/place-cont.js
+++ controlers/place-cont.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
 const Place = require('../models/place');
-const Users = require('../Models/user');
+const Users = require('../models/user');
 
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
