67e3266e8038ede0c84a8c0fea563ed7aeb51678
lorgical-mind <103314852+lorgical-mind@users.noreply.github.com>
2023-08-18 22:59:39 +0530

    app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use("/uploads/photos",express.static(path.join('uploads','photos')));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://sharemytrip-223.web.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://sharemytrip-90c79.web.app');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
