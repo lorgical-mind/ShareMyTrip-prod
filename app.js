75a91ae96ee3bf3c44053835d38f44dd8b0c2dab
lorgical-mind <103314852+lorgical-mind@users.noreply.github.com>
2023-08-19 09:02:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://sharemytrip-90c79.web.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
